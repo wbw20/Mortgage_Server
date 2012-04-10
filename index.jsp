b2114df3ed93c90f9f6bf2776ae0d2e765ec37eb
William B. Wettersten <wbw20@case.edu>
2012-04-10 21:35:03 +0000

    NOW we are playing by tomcats rules

---
+++ index.jsp
@@ -0,0 +1,8 @@
+<html>
+<head>
+<title>Mortgage Calculator</title>
+</head>
+<body>
+<h1>welcome!</h1>
+</body>
+</html>
--- src/MortgageServlet.java
+++ src/MortgageServlet.java
@@ -1,26 +1,36 @@
 import java.io.*;
-import java.servlet.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
 
 public class MortgageServlet extends HttpServlet
 {
 	public void doGet(HttpServletRequest request, HttpServletResponse response)
 	{
-		response.setConentType("text/html");
-		PrintWriter out = response.getWriter();
+		try
+		{
+			PrintWriter out = response.getWriter();
 
-		out.println("hello world!");
-		out.close();
+			out.println("hello world!");
+			out.close();
+		}
+		catch(IOException e)
+		{
+			System.err.print("an error has occured on GET");
+		}
 	}
 
-	public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+	public void doPost(HttpServletRequest request, HttpServletResponse response)
 	{
-		response.setContentType("text/html");
-		PrintWriter out = response.getWriter();
+		try
+		{
+			PrintWriter out = response.getWriter();
 
-		out.println("<title>Mortage Calculator</title>");
-
-		//TODO:  get input and call backend functions
-
-		out.close();
+			out.println("<title>Mortage Calculator</title>");
+			out.close();
+		}
+		catch(IOException e)
+		{
+			System.err.print("an error has occured on POST");
+		}
 	}
 }
